f225e6a8ac5c98f2f7694c2b54c3d060cd5b02c9
julian_sa <julian.sauter@posteo.de>
2024-01-17 12:14:25 +0100

    plot actual data for different time frames

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,14 +5,21 @@ import pandas as pd
 raw_data_path = r'C:\Users\Latitude\Desktop\Kaggle\time_series_energy_portfolio_project\data\raw\RealTimeConsumption-01012016-04082020.csv'
 df = pd.read_csv(raw_data_path)
 df.head(300)
-df.tail()
-df.set_index('Date', inplace=True)
+
+# change the column name date to datetime
+df.rename(columns={'Date': 'Datetime'}, inplace=True)
+
+# integrate hour column with date column ()
+df['Datetime'] = df['Datetime'] + ' ' + df['Hour']
+
+# drop Hour column
+df.drop(['Hour'], axis=1, inplace=True)
+
+# set Datetime column as index
+df.set_index('Datetime', inplace=True)
 
 # change index to datetime
-df.index = pd.to_datetime(df.index, format="%d.%m.%Y")
-
-# change hour column type to int
-#df['Hour'] = df['Hour'].astype(int)
+df.index = pd.to_datetime(df.index, format="%d.%m.%Y %H:%M")
 
 # change Consumption (MWh) column type to float (first remove commas)
 df['Consumption (MWh)'] = df['Consumption (MWh)'].str.replace(',', '')
@@ -24,11 +31,6 @@ df = df[df['Consumption (MWh)'] > 1000]
 
 # remove 2020 data
 df = df[df.index.year < 2020]
-
-# build features day_of_week, day_of_month, month
-df['year'] = df.index.year
-df['day_of_month'] = df.index.day
-df['month'] = df.index.month
 
 # save data frame to data/processed
 processed_data_path = r'C:\Users\Latitude\Desktop\Kaggle\time_series_energy_portfolio_project\data\processed\RealTimeConsumption-01012016-31122019.csv'
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -3,9 +3,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # read processed data
-processed_data_path = r'C:\Users\Latitude\Desktop\Kaggle\time_series_energy_portfolio_project\data\processed\RealTimeConsumption-01012016-04082020.csv'
+processed_data_path = r'C:\Users\Latitude\Desktop\Kaggle\time_series_energy_portfolio_project\data\processed\RealTimeConsumption-01012016-31122019.csv'
 df = pd.read_csv(processed_data_path, index_col=0)
+df.tail()
 
-# plot data
+# plot Consumption (MWh) column from 2016-01-01 to 2020-08-04
 color_pal = sns.color_palette()
-df.plot(style='.', figsize=(15,5), color=color_pal[0], title='Energy Consumption in MWh')
+df['Consumption (MWh)'].plot(style='.', figsize=(15,5), color=color_pal[0], title='Energy Consumption in MWh')
+
+# plot Consumption (MWh) column for the an exemplary week in 2016 a line plot and dot plot
+df['Consumption (MWh)'].loc['2016-01-04':'2016-01-10'].plot(figsize=(15,5), title='Energy Consumption in MWh')
+df['Consumption (MWh)'].loc['2016-01-04':'2016-01-10'].plot(style='.', figsize=(15,5), color=color_pal[0], title='Energy Consumption in MWh')
+
+# plot data for the an exemplary month in 2016 a line plot
+df['Consumption (MWh)'].loc['2016-01-01':'2016-01-31'].plot(figsize=(15,5), title='Energy Consumption in MWh')
