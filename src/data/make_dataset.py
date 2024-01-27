71dd2ba1465c48e3ed44931433fb2dfd8f4e0514
julian_sa <julian.sauter@posteo.de>
2024-01-27 12:49:56 +0100

    outliers

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,6 +6,7 @@ raw_data_path = r'C:\Users\Latitude\Desktop\Kaggle\time_series_energy_portfolio_
 df = pd.read_csv(raw_data_path)
 df.head(300)
 
+
 # change the column name date to datetime
 df.rename(columns={'Date': 'Datetime'}, inplace=True)
 
@@ -21,16 +22,23 @@ df.set_index('Datetime', inplace=True)
 # change index to datetime
 df.index = pd.to_datetime(df.index, format="%d.%m.%Y %H:%M")
 
+# remove 2020 data
+df = df[df.index.year < 2020]
+
 # change Consumption (MWh) column type to float (first remove commas)
 df['Consumption (MWh)'] = df['Consumption (MWh)'].str.replace(',', '')
 df['Consumption (MWh)'] = df['Consumption (MWh)'].astype(float)
 df.info()
 
-# remove Consumption (MWh) values under 1000 since they are errors
-df = df[df['Consumption (MWh)'] > 1000]
+## Outlier analysis and removal
+# plot histogram of consumption data
+df['Consumption (MWh)'].hist(bins=500)
 
-# remove 2020 data
-df = df[df.index.year < 2020]
+# have a closer look at lower values with dot plot
+df[df['Consumption (MWh)'] < 20000].plot(style='.', figsize=(15,5))
+
+# remove Consumption (MWh) values under 1000 since they certainly are errors
+df = df[df['Consumption (MWh)'] > 1000]
 
 # save data frame to data/processed.
 processed_data_path = r'C:\Users\Latitude\Desktop\Kaggle\time_series_energy_portfolio_project\data\processed\RealTimeConsumption-01012016-31122019.csv'
